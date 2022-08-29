fa5666491fb5f6cb0ef0333ada9be6e254970fec
Jerson Galvez <jerson.galvez@pijaossalud.com.co>
2022-08-29 13:29:09 -0500

    subiendo los cambios para entrega y dejar al dia

---
+++ vista/Dashboard.php
@@ -0,0 +1,5 @@
+<iframe
+  title="PresupuestoMaximo - General" width="1300"
+  height="600" src="https://app.powerbi.com/view?r=eyJrIjoiODhiYzliNjEtNjllNi00MjIzLWIyZTItZGZjZjg3ODkyMjUzIiwidCI6IjVkMDcxOGUyLTU5MjEtNDI0NS05OGU1LWQ1ZTM5MDZmNGVlYiJ9"
+  frameborder="0" allowFullScreen="true">
+</iframe>
